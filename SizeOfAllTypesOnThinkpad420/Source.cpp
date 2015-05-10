9bb774fecf29ed1eb44cd2fbb90c84d316124a50
biggreyhairboy <biggreyhairboy@gmail.com>
2015-05-10 23:45:53 +0800

    length of types is strange

---
+++ SizeOfAllTypesOnThinkpad420/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "size of bool is: " << sizeof(bool) << endl;
+	cout << "size of char is: " << sizeof(char) << endl;
+	cout << "size of wchar_t is: " << sizeof(wchar_t) << endl;
+	cout << "size of short is: " << sizeof(short) << endl;
+	cout << "size of int is: " << sizeof(int) << endl;
+	cout << "size of long is: " << sizeof(long) << endl;
+	cout << "size of float is: " << sizeof(float) << endl;
+	cout << "size of double is: " << sizeof(double) << endl;
+	cout << "size of long double is: " << sizeof(long double) << endl;
+	getchar();
+
+
+}
